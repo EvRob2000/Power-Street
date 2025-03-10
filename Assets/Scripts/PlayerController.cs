7b9661f679949cc24dfe26525f008b05b6338efa
EvRob2000 <evrob2000@outlook.com>
2025-03-10 12:42:08 -0400

    animation progress

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Enums
+    private enum Directions { UP, DOWN, LEFT, RIGHT }
+    #endregion
+
     #region Editor Data
     [Header("Movement Attributes")]
     [SerializeField] float _moveSpeed = 50f;
 
     [Header("Dependencies")]
     [SerializeField] Rigidbody2D _rb;
+    [SerializeField] Animator _animator;
+    [SerializeField] SpriteRenderer _spriteRenderer;
     #endregion
 
     #region Internal Data
     private Vector2 _moveDir = Vector2.zero;
+    private Directions _facingDirection = Directions.RIGHT;
     #endregion
 
     #region Tick
@@ -35,15 +42,30 @@ public class PlayerController : MonoBehaviour
     {
         _moveDir.x = Input.GetAxisRaw("Horizontal");
         _moveDir.y = Input.GetAxisRaw("Vertical");
-
-        Debug.Log(_moveDir);
     }
     #endregion
 
     #region Movement Logic
     private void MovementUpdate()
     {
-        _rb.velocity = _moveDir * _moveSpeed * Time.fixedDeltaTime;
+        _rb.velocity = _moveDir.normalized * _moveSpeed * Time.fixedDeltaTime;
+    }
+    #endregion
+
+    #region Animation Logic
+    private void CalculateFacingDirection()
+    {
+        if (_moveDir.x != 0)
+        {
+            if (_moveDir.x > 0) //Moving right
+            {
+                _facingDirection = Directions.RIGHT;
+            }
+            else if (_moveDir.x < 0) //Moving left
+            {
+                _facingDirection= Directions.LEFT;
+            } 
+        }
     }
     #endregion
 }
