ed275a9b647c5e8f7593631cf04c614d3ba9ecd3
EvRob2000 <evrob2000@outlook.com>
2025-04-02 18:28:19 -0400

    things and such

--- Assets/Scripts/UI Scripts/UIController.cs
+++ Assets/Scripts/UI Scripts/UIController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class UIController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1;
         isGamePaused = false;
         pauseScreen.SetActive(false);
     }
@@ -34,6 +36,11 @@ public class UIController : MonoBehaviour
             isGamePaused = false;
             Time.timeScale = 1;
         }
+    }
+
+    public void SceneLoad(string scene)
+    {
+        SceneManager.LoadScene(scene);
     }
 
     public void Quit()
